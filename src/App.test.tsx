c055c6bb62f6180d1a70a3c3c8405cfd11560336
timmae219 <timerdar@googlemail.com>
2023-06-23 19:52:28 +0200

    Performs an initial project cleanup to remove the default contents, includes assets and fonts

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,3 +1,7 @@
+/*
+
+The standard test file for the default React start app looked like this:
+
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
@@ -6,4 +10,6 @@ test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
-});
+});*/
+
+export {}
