c4fc3873c9f4e9ebeb73972a7598797f1b73510e
timmae219 <timerdar@googlemail.com>
2024-11-16 22:36:13 +0100

    fixes unit tests to be consistent to current node standards

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -2,11 +2,28 @@ import React from 'react';
 import { render, RenderResult } from "@testing-library/react";
 import App from '../App';
 import '@testing-library/jest-dom';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import rootReducer from '../redux/rootReducer';
+
+const store = configureStore({
+  reducer: rootReducer,
+  preloadedState: {
+    posts: {
+      posts: [],
+      status: 'idle',
+      error: null
+    }
+  }
+});
 
 describe('Root App Component Tests', () => {
   it('renders header correctly when rendering app component', () => {
-
-    const { getByTestId }: RenderResult = render(<App />);
+    const { getByTestId }: RenderResult = render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
     const cometHeader: HTMLElement = getByTestId('comet-header');
 
     expect(cometHeader).toBeInTheDocument();
--- src/redux/actions.tsx
+++ src/redux/actions.tsx
@@ -1,8 +1,4 @@
-import { AsyncThunkAction } from "@reduxjs/toolkit";
-import { GET_POPULAR_POSTS } from "./actionTypes";
 import { fetchPosts } from "./reducers/posts";
-import { AsyncThunkConfig } from "@reduxjs/toolkit/dist/createAsyncThunk";
-import Post from "../models/post";
 
 export class Action {
     type: string;
