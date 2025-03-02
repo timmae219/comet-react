7e0c83d99644da26fdbcfc83106a6e31315adb93
timmae219 <timerdar@googlemail.com>
2025-03-02 21:49:07 +0100

    remove any types

--- src/models/post.tsx
+++ src/models/post.tsx
@@ -27,17 +27,24 @@ export default class Post {
     this.comments = comments;
   }
 
-  static fromJson(json: any): Post {
+  static fromJson(json: {
+    data: {
+      title: string;
+      author: string;
+      subreddit_name_prefixed: string;
+      selftext_html: string;
+      preview?: { images: { source: { url: string } }[] };
+      ups: number;
+    };
+  }): Post {
     // TODO: parse comments
     return new Post(
-      json["data"]["title"],
-      json["data"]["author"],
-      json["data"]["subreddit_name_prefixed"],
-      json["data"]["selftext_html"],
-      json["data"]["preview"]
-        ? json["data"]["preview"]["images"][0]["source"]["url"]
-        : null,
-      json["data"]["ups"],
+      json.data.title,
+      json.data.author,
+      json.data.subreddit_name_prefixed,
+      json.data.selftext_html,
+      json.data.preview ? json.data.preview.images[0].source.url : null,
+      json.data.ups,
       [],
     );
   }
